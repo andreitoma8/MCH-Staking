842f9c67904ad328f134ceb1fe891b5d50b7b5b3
andreitoma8 <andrei.toma8@yahoo.com>
2022-05-01 19:11:48 +0300

    main

--- contracts/MoonChessToken.sol
+++ contracts/MoonChessToken.sol
@@ -5,33 +5,94 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MoonChessToken is
     ERC20,
     ERC20Burnable,
-    ERC20Permit,
     ERC20Votes,
-    Ownable
+    Ownable,
+    KeeperCompatibleInterface
 {
-    address private team;
-    address private liquidity;
-    address private marketing;
-    address private presale;
-    address private ecosystem;
-    address private rewards;
+    // The interval for minting new tokens for Game & Staking Rewards
+    uint256 public immutable interval;
+
+    // The timestamp of the latest execution of ChainLink Keepers
+    uint256 public lastTimeStamp;
+
+    // The time of start for minting staking and game rewards
+    uint256 public firstTimeStamp;
+
+    // This will be turned on when staking is enabled. It cannot be turned off later, even by the owner.
+    bool public stakingStarted;
+
+    // Address of the Staking Pool
+    address public stakingPool;
+
+    // Address of the Game to send rewards tokens to
+    address public gameAddress;
 
     // Mint 100.000.000.000 Tokens on contract deployment
     constructor() ERC20("MoonChess", "MCH") ERC20Permit("MoonChess") {
-        // _mint(team, 300000000000);
-        // _mint(liquidity, 100000000000);
-        // _mint(marketing, 100000000000);
-        // _mint(presale, 200000000000);
-        // _mint(ecosystem, 150000000000);
-        // _mint(rewards, 150000000000);
         _mint(msg.sender, 1000000000000 * 10**18);
+        interval = 21600;
     }
+
+    // Keepers function that checks if Upkeep action is needed
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    )
+        external
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        upkeepNeeded =
+            (block.timestamp - lastTimeStamp) > interval &&
+            stakingStarted;
+        // We don't use the checkData in this example. The checkData is defined when the Upkeep was registered.
+    }
+
+    // Function called by ChainLink Upkeeper
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        require((block.timestamp - lastTimeStamp) > interval && stakingStarted);
+        lastTimeStamp = block.timestamp;
+        _mint(stakingPool, mchToMint(1600000));
+        _mint(gameAddress, mchToMint(2400000));
+    }
+
+    // Enables minting rewards for the staking pool and game rewards
+    function startStaking() external onlyOwner {
+        stakingStarted = true;
+        firstTimeStamp = block.timestamp;
+    }
+
+    // Set the address of the staking pool
+    function setStakingPool(address _stakingPool) external onlyOwner {
+        stakingPool = _stakingPool;
+    }
+
+    // Set the address of the game
+    function setGameAddress(address _game) external onlyOwner {
+        gameAddress = _game;
+    }
+
+    // Internal function
+
+    // Calculates the amount to mint per 6h for the Game and Staking rewards, based on the mining schedule
+    function mchToMint(uint256 _amount) internal view returns (uint256) {
+        return ((_amount * 10**18) /
+            (2**((block.timestamp - firstTimeStamp) / 31556926)) +
+            1);
+    }
+
+    // Overrides
 
     function _afterTokenTransfer(
         address from,
@@ -54,4 +115,6 @@ contract MoonChessToken is
     {
         super._burn(account, amount);
     }
+
+    //1600000 tokens per 6h
 }
